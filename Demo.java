90f08d42123fadbeea030116725b78f9b15f50df
Yashika Khatri <yashikakhatri@Yashikas-MacBook-Pro.local>
2021-02-09 16:01:03 -0500

    set value of a to 100

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Consultadd");
-        int a=50;
+        int a=100;
         int b=26;
         System.out.println("result" + (a*b));
         System.out.println("multiplication");
