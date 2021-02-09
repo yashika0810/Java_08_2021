ef18885e0f5e7453ce817a6579a788c30827c046
yashika0810 <48493052+yashika0810@users.noreply.github.com>
2021-02-09 15:52:01 -0500

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,10 +1,10 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Consultadd");
-        int a=100;
+        int a=50;
         int b=26;
-        System.out.println("result" + (a+b));
-        System.out.println("added");
+        System.out.println("result" + (a*b));
+        System.out.println("multiplication");
         System.out.println("Hello welcome to the training");
     }
 }
